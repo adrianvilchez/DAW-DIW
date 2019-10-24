33f9352e5a71980f027f2e3d08f1b7175f8452a4
adrianvilchez <avilchez758@gmail.com>
2019-10-24 20:36:38 +0200

    Comportamiento de columnas, se puede ser más cerdo?

--- UD02/uncthuled/uncthuled.js
+++ UD02/uncthuled/uncthuled.js
@@ -1,6 +1,9 @@
 // Array del mapa
 var mapa;
 
+var posicionesPrimeraColumnaX;
+var posicionesPrimeraColumnaY;
+
 // Posición del personaje
 var xet = 1;
 var yet = 8;
@@ -8,22 +11,32 @@ var yet = 8;
 var xpredator = 14;
 var ypredator = 20;
 
+var xinicioColumnas;
+var yinicioColumnas;
+
 var vidas = 5;
 
 velocidadMomia = 500;
 
 var personajeEnTablero = true;
 
+
+
 window.onload = function() {
     var divCuadricula;
 
     mapa = new Array(16);
+
+    posicionesPrimeraColumnaX = new Array();
+    posicionesPrimeraColumnaY = new Array();
 
     for (let index = 0; index < mapa.length; index++) {
         mapa[index] = new Array(23);   
     }
 
     cuadricula(23, 16);
+
+    comprobarCajas();
 
     setInterval(() => {
         // Comentamos para que no de por culo
@@ -79,44 +92,72 @@ document.addEventListener('keydown', function(event) {
             }
             break;
     }
-    comprobarCajas();
+
     
-    comprobarColumnasAdyecentes();
+    marcarColumnasAdyecentes();
+
+    //comprobarColumna(3, 10);
+    obtenerColumnas();
 
 });
 
-// Comprobamos si hay columans adyacentes al personaje
-function comprobarColumnasAdyecentes() {
+function obtenerColumnas() {
     
-    // Comprobar si hay columna a la derecha
-    if (mapa[xet][yet + 1].className.indexOf("columna") >= 0) {
-        mapa[xet][yet + 1].classList.remove("columnas");
-        mapa[xet][yet + 1].classList.add("columnasAdyacentes");
+    for (let i = 0; i < 20; i++) {
+        
+        //alert(posicionesPrimeraColumnaX[i]);
+        //alert(posicionesPrimeraColumnaY[i]);
+        comprobarColumna(parseInt(posicionesPrimeraColumnaX[i]), parseInt(posicionesPrimeraColumnaY[i]));
+        
     }
+}
 
-    // Comprobar si hay columna a la izquierda
-    if (mapa[xet][yet - 1].className.indexOf("columna") >= 0) {
-        mapa[xet][yet - 1].classList.remove("columnas");
-        mapa[xet][yet - 1].classList.add("columnasAdyacentes");
+function comprobarColumna(x, y) {
+    
+    if (mapa[x][y].classList.contains("rodeada")
+    && mapa[x][y+1].classList.contains("rodeada")
+    && mapa[x][y+2].classList.contains("rodeada")
+    && mapa[x+1][y].classList.contains("rodeada")
+    && mapa[x+1][y+1].classList.contains("rodeada")
+    && mapa[x+1][y+2].classList.contains("rodeada")
+    
+    && mapa[x][y-1].classList.contains("pisadas")
+    && mapa[x+1][y-1].classList.contains("pisadas")
+    && mapa[x+2][y].classList.contains("pisadas")
+    && mapa[x+2][y+1].classList.contains("pisadas")
+    && mapa[x+2][y+2].classList.contains("pisadas")
+    && mapa[x+1][y+3].classList.contains("pisadas")
+    && mapa[x][y+3].classList.contains("pisadas")
+    && mapa[x-1][y+3].classList.contains("pisadas")
+    && mapa[x-1][y+2].classList.contains("pisadas")
+    && mapa[x-1][y].classList.contains("pisadas")) {
+        console.log("Los 6 divs contienen la clase 'rodeada'.  " + x + "    " + y);
+
+        
     }
+}
 
-    // Comprobar si hay columna debajo
-    if (mapa[xet + 1][yet].className.indexOf("columna") >= 0) {
-        mapa[xet + 1][yet].classList.remove("columnas");
-        mapa[xet + 1][yet].classList.add("columnasAdyacentes");
+// Comprobamos si hay columans adyacentes al personaje
+function marcarColumnasAdyecentes() {
+    
+    if (mapa[xet + 1][yet].className.indexOf("columna") >= 0
+        || mapa[xet - 1][yet].className.indexOf("columna") >= 0
+        || mapa[xet][yet + 1].className.indexOf("columna") >= 0
+        || mapa[xet][yet - 1].className.indexOf("columna") >= 0) {
 
-    }
-
-    // Comprobar si hay columna a la arriba
-    if (mapa[xet - 1][yet].className.indexOf("columna") >= 0) {
-        mapa[xet - 1][yet].classList.remove("columnas");
-        mapa[xet - 1][yet].classList.add("columnasAdyacentes");
+        mapa[xet+1][yet].classList.add("rodeada");
+        mapa[xet][yet+1].classList.add("rodeada");
+        mapa[xet-1][yet].classList.add("rodeada");
+        mapa[xet][yet-1].classList.add("rodeada");
+        
     }
 }
 
 
 function moverMomia() {
 
+    mapa[14][21].classList.remove("momia");
+    
     if (mapa[xpredator-1][ypredator].className.indexOf("pasillo") >= 0) {
         buscandoAEt();
     } else if (mapa[xpredator][ypredator-1].className.indexOf("pasillo") >= 0) {
@@ -139,6 +180,7 @@ function moverMomia() {
 }
 
 function buscandoAEt() {
+
     if (xpredator < xet) {
         mapa[xpredator][ypredator].classList.remove("momia");
         mapa[xpredator][ypredator].classList.add("pasillo");
@@ -195,17 +237,37 @@ function comprobarCajas() {
     
     let bloquesColumnas = document.querySelectorAll(".columnas");
 
+    let bloqueColumnas;
+
     console.log(bloquesColumnas.length);
 
-    for (let i = 0; i < 120; i++) {
+    for (let i = 0; i < bloquesColumnas.length; i++) {
 
-        if (i % 3 == 0) {
-            
-            
+        bloqueColumnas = bloquesColumnas[i].getAttribute("data-indice");
+
+        // Calculamos si el bloque está en una línea par
+        if (parseInt(i / 15) % 2 == 0) {
+          
+            if (i % 3 == 0) {
+                
+                posicionesPrimeraColumnaX.push(bloquesColumnas[i].getAttribute("data-x"));
+                posicionesPrimeraColumnaY.push(bloquesColumnas[i].getAttribute("data-y"));
+            }  
         }
+
     }
 
-    
+
+
+    /*while (mapa[xinicioColumnas][yinicioColumnas - 1].classList.contains("columnas")) {
+        
+        xinicioColumnas--;
+
+        while (mapa[xinicioColumnas - 1][yinicioColumnas].classList.contains("columnas")) {
+
+            yinicioColumnas--;
+        }
+    }*/
 }
 
 function cuadricula(ancho, alto) {
@@ -265,9 +327,14 @@ function cuadricula(ancho, alto) {
             }*/
 
             divCuadricula.innerHTML = i + " - " + j;
+
+            divCuadricula.dataset.x = i;
+            divCuadricula.dataset.y = j;
             document.getElementById("contenedor").appendChild(divCuadricula);
             
             mapa[i][j] = divCuadricula;
+
+
 
             contador++;
         }
