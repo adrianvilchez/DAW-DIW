8ef2223b09398d7a5ee9b8c595afba1414110a8d
adrianvilchez <avilchez758@gmail.com>
2019-11-24 16:38:33 +0100

    [MIGRACIÓN] - barra de progreso, falta adecentarla

--- UD03/js/migracion.js
+++ UD03/js/migracion.js
@@ -14,48 +14,39 @@ FUNCIONES PERDIDAS
 
 */
 
+function hacerNonos(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 async function startMigration(){
 
     // Fragmentos perdidos
     // ^(;,;)^
 
     
-
-    
-    barrasProgreso();
-}
-
-async function barrasProgreso() {
-
     let barrasProgreso = document.querySelectorAll("progress");
 
-    console.log(barrasProgreso);
 
+    for (let i = 0; i < barrasProgreso.length; i++) {
+
+        for (let j = 0; j < 100; j++) {
+
+            let valorProgreso = barrasProgreso[i].value;
+            //let valorProgreso = progreso.value;
     
+            barrasProgreso[i].value += j;
+            //progreso.value += j;
 
-    barrasProgreso.forEach(progreso => {
-
-        for (let i = 0; i < 100; i++) {
-
-            let valorProgreso = progreso.value;
-    
-            console.log(valorProgreso);
-    
-            progreso.value += i;
-
-            await delay(500);
+            await hacerNonos(50);
         }
-    });
-
-
-
+    }
 }
 
-function init(){
+async function init(){
     console.info(" * Init envirnoment ");
 
     // Set click function on button
-    document.querySelector("button").addEventListener("click",startMigration);
+    document.querySelector("button").addEventListener("click", startMigration);
 }
 
 // Init the environment when all is ready
