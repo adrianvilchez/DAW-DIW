6291ba699c03b7454c043f3e7bdf2e5dd7ed7f1c
adrianvilchez <avilchez758@gmail.com>
2019-12-10 20:20:57 +0100

    Modificación para que el script capte las cámaras de tráfico

--- UD04_Formularios/public/js/buscar.js
+++ UD04_Formularios/public/js/buscar.js
@@ -1,46 +1,67 @@
+const camarasUrl = "http://mapas.valencia.es/lanzadera/opendata/Tra-camaras/JSON";
 
-const fuentesUrl = "http://mapas.valencia.es/lanzadera/opendata/cia_fuentes/JSON";
+// Esta es la funcion de filtrado para
+// obtener tan solo los elementos que cumplen
+// una serie de requisitos.
 
-
-function filtroLetra(elemento){
-    //console.log(elemento);
-    let letra=document.querySelector(`input[name="calle"]`).value;
-    return elemento.properties.calle.startsWith(letra);
+function filtroLetra(elemento) {
+	//console.log(elemento.properties);
+    let letra = document.querySelector(`input[name="calle"]`).value;
+    return elemento.properties.descripcio.startsWith(letra);
 }
 
-function toUpp(){
-    document.querySelector(`input[name="calle"]`).value=document.querySelector(`input[name="calle"]`).value.toUpperCase();
+
+// Pasa a mayuscula el texto de propio input
+// se lanza cada vez que se realiza una insercion en
+// el texto del nombre.
+function toUpp() {
+    document.querySelector(`input[name="calle"]`).value = document.querySelector(`input[name="calle"]`).value.toUpperCase();
 }
 
+// api ETRS89 / UTM zone 30N to WGS84
 function buscar(){
 
-    const fetchPromesa = fetch(fuentesUrl);
+    // Obtenemos el JSON que esta definido
+    const fetchPromesa = fetch(camarasUrl);
 
+    // Cuando se resuelva la promesa
     fetchPromesa.then(response => {
-	return response.json();
-    }).then(respuesta =>{
-	const resultado=respuesta.features.filter(filtroLetra);
-	console.log(resultado);
+		// la pasamos a JSON
+		return response.json();
+		// Y entonces
+    }).then(respuesta => {
+		// Filtramos los resultados con el filtro definido anteriormente
 
-	let listado=document.createElement("ul");
-	resultado.forEach(fuente=>{
-	    console.log(fuente.properties.calle);
-	    let calleli=document.createElement("li");
-	    calleli.innerHTML=fuente.properties.calle+" -- ["+fuente.geometry.coordinates+"]";
-	    listado.appendChild(calleli);	    
-	});
-	document.querySelector(".resultados").innerHTML="";
-	document.querySelector(".resultados").appendChild(listado);
+		console.log(respuesta.features[1].properties);
+		const resultado = respuesta.features.filter(filtroLetra);
+
+		// Una vez tenemos el listado filtrado pasamos a crear
+		// cada uno de los <li> que representan
+		let listado = document.createElement("ul");
+
+		// Por cada uno de ellos 
+		resultado.forEach(camara => {
+			// Creamos un <li>
+			let calleli = document.createElement("li");
+			calleli.innerHTML = camara.properties.descripcio + " -- [" + camara.geometry.coordinates + "]";
+			// Lo anyadimos
+			listado.appendChild(calleli);	    
+		});
+		// Establecemos el listado en la Web
+		document.querySelector(".resultados").innerHTML = "";
+		document.querySelector(".resultados").appendChild(listado);
     });
-
 }
 
-function init(){
+function init() {
 
-    document.querySelector(`input[type="button"]`).addEventListener("click",buscar);
-    document.querySelector(`input[type="text"]`).addEventListener("input",toUpp);
+    // Binding de los eventos correspondientes.
+
+    // Click en el boton de buscar
+    document.querySelector(`input[type="button"]`).addEventListener("click", buscar);
+    // Texto cambia en el <input>
+    document.querySelector(`input[type="text"]`).addEventListener("input", toUpp);
 }
 
-
-window.onload=init;
-    
+// The mother of the lamb.
+window.onload = init;
